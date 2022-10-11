34d579e1c01a4ce947f219cbad3bd1550521930e
felipe neres <f3neres@gmail.com>
2022-10-11 14:00:16 -0300

    prova2

--- src/App.js
+++ src/App.js
@@ -5,24 +5,43 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
+// Ex1
   const [qtdGrande, setQtdGrande] = useState (0);
   const [qtdMedio, setQtdMedio] = useState (0);
   const [qtdPqno, setQtdPqno] = useState (0);
   const [cumpom, setCumpom] = useState (0);
   const [resposta, setResposta] = useState (0);
 
-
+// Ex2
   const [mes, setMes] = useState ('');
-  const [dia, setDia] = useState ();
+  const [dia, setDia] = useState (1);
   const [libra, setLibra] = useState (false);
 
+// Ex3
   const [gramas, setGramas] = useState ();
-  const [valor, setValor] = useState (0);
+  const [valor, setValor] = useState ();
+
+// Ex4
+  const [salario, setSalario] = useState ();
+  const [bonus, setBonus] = useState ();
+  const [desc, setDesc] = useState ();
+  const [sLiquido, setSLiquido] = useState ();
+
+// Ex5
+  const [tanque, setTanque] = useState ();
+  const [dist, setDist] = useState ();
+  const [consumo, setConsumo] = useState ();
+  const [viagem, setViagem] = useState ();
+
+// Ex6
+  const [temp, setTemp] = useState ();
+  const [febre, setFebre] = useState ();
+
   
 
 
 
-
+// Ex1
     function calcular(){
       let qtd = (qtdPqno * 13.50) + (qtdMedio * 15) + (qtdGrande * 17.50);
       let desconto = (cumpom  / 100) * qtd ;
@@ -30,7 +49,7 @@ function App() {
 
       setResposta(total)
     }
-
+// Ex2
     function signo(){
 
       let resp = false
@@ -58,12 +77,11 @@ function App() {
       else{
         resp = false
       }
-      console.log(resp)
       setLibra(resp)
 
     }
 
-
+// Ex3
     function Sorveteria(){
       let total = 0
 
@@ -77,6 +95,47 @@ function App() {
 
     }
 
+  // Ex4
+    function Salario(){
+      let cBonus = salario + ((bonus/100) * salario);
+      let resp = cBonus - desc;
+      setSLiquido(resp)
+    }
+
+  // Ex5
+    function Carro(){
+      let kmLitro = consumo / dist
+      let resp = kmLitro / tanque
+      
+      setViagem(Math.ceil(resp))
+    }
+    
+  // Ex6
+    function calcFebre(){
+      let resp = ""
+
+      if (temp >= 41) {
+        resp = "Hiperfermia"
+      }
+
+      else if (temp >= 39.6) {
+        resp = "Febre alta"
+      }
+
+      else if (temp >= 37.6) {
+        resp = "Febre"
+      }
+
+      else if (temp >= 36) {
+        resp = "Normal"
+      }
+
+      else if (temp <= 36) {
+        resp = "Hipotermia"
+      }
+
+      setFebre(resp)
+    }
 
 
 
@@ -88,21 +147,21 @@ function App() {
        <div> 
           <h1>açaiaria</h1>
 
-          <h1>Quantidade de pequenos</h1>
+          <h2>Quantidade de pequenos</h2>
           <input type= "number" value={qtdPqno} onChange={e => setQtdPqno (Number(e.target.value))} />
 
-          <h1>Quantidade de medios</h1>
+          <h2>Quantidade de medios</h2>
           <input type= "number" value={qtdMedio} onChange={e => setQtdMedio (Number(e.target.value))} />
 
-          <h1>Quantidade de grandes</h1>
+          <h2>Quantidade de grandes</h2>
           <input type= "number" value={qtdGrande} onChange={e => setQtdGrande (Number(e.target.value))} />
 
-          <h1>desconto</h1>
+          <h2>desconto</h2>
           <input type= "number" value={cumpom} onChange={e => setCumpom (Number(e.target.value))} />
 
           <button onClick={calcular}>Comprar</button>
 
-          <h1>{resposta}</h1>
+          <h2>{resposta}</h2>
 
         </div>
         <hr></hr>
@@ -111,16 +170,16 @@ function App() {
 
             <h1>tu é di libra é?</h1>
 
-            <h1>Seu mes</h1>
+            <h2>Seu mes</h2>
           <input type= "text" value={mes} onChange={e => setMes (e.target.value)} />
 
           
-          <h1>Seu dia</h1>
-          <input type= "text" value={dia} onChange={e => setDia (e.target.value)} />
+          <h2>Seu dia</h2>
+          <input type= "text" value={dia} onChange={e => setDia (Number(e.target.value))} />
 
           <button onClick={signo}>Testar</button>
 
-          <h1>é de libra? :{libra ? 'Sim' : 'Não'}</h1>
+          <h2>é de libra? :{libra ? 'Sim' : 'Não'}</h2>
 
         </div>
 
@@ -130,19 +189,79 @@ function App() {
             <h1>Sorvetin pro Bruno</h1>
             <h2>Desconto apartir de 1000g/1kg</h2>
 
-            <h3>Quantas gramas você deseja?</h3>
+            <h2>Quantas gramas você deseja?</h2>
             <input type= "number" value={gramas} onChange={e => setGramas (Number(e.target.value))} />
 
             <button onClick={Sorveteria}>Comprar</button>
 
-            <h1>total a pagar é: R${valor}</h1>
+            <h2>total a pagar é: R${valor}</h2>
 
+        </div>
+
+        <br></br>
+        <hr></hr>
+        <h1 className='prova'>PROVA 2</h1>
+
+
+        <div> 
+          <h1>Salario</h1>
+
+          <h2>Seu salario</h2>
+          <input type= "number" value={salario} onChange={e => setSalario (Number(e.target.value))} />
+
+          <h2>Bonus do salario</h2>
+          <input type= "number" value={bonus} onChange={e => setBonus (Number(e.target.value))} />
+
+          <h2>Desconto do salario</h2>
+          <input type= "number" value={desc} onChange={e => setDesc (Number(e.target.value))} />
+
+          
+          <button onClick={Salario}>Calcular</button>
+
+          <h2>{sLiquido}</h2>
+
+        </div>
+
+        <hr></hr>
+
+        <div> 
+          <h1>Viagem</h1>
+
+          <h2>Tamanho do tanque</h2>
+          <input type= "number" value={tanque} onChange={e => setTanque (Number(e.target.value))} />
+
+          <h2>Distancia da viagem</h2>
+          <input type= "number" value={dist} onChange={e => setDist (Number(e.target.value))} />
+
+          <h2>Consumo do deu veiculo</h2>
+          <input type= "number" value={consumo} onChange={e => setConsumo (Number(e.target.value))} />
+
+          
+          <button onClick={Carro}>Calcular</button>
+    
+          <h2>{viagem}</h2>
+
+        </div>
+
+        <hr></hr>
+
+        <div>
+          <h1>Está com febre</h1>
+
+            <h2>Qual sua temperatura?</h2>
+            <input type= "number" value={temp} onChange={e => setTemp (Number(e.target.value))} />
+
+            <button onClick={calcFebre}>Testar</button>
+
+            <h2>Voce esta com: {febre}</h2>
         </div>
 
 
 
     </main>
+
   );
+
 }
 
 export default App;
